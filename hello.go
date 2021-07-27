e89dcf6b0b925b06cc53cf8905e8a8c28c049636
Ricardo12138 <754678483@qq.com>
2021-07-27 13:23:24 +0800

    branch test

--- hello.go
+++ hello.go
@@ -1,7 +1,137 @@
 package main
-import "fmt"
+
+//gittest
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Proto struct {
+	Name        string
+	RepoURL     string
+	ID          int64
+	MessageList []*Message
+	ServiceList []*Service
+}
+
+type Message struct {
+	Name      string
+	FieldList []*Field
+}
+
+type Field struct {
+	Name     string
+	Type     string
+	ID       int64
+	Repeated bool
+}
+
+type Service struct {
+	Name string
+	Rpcs []*Rpc
+}
+
+type Rpc struct {
+	Name       string
+	Req        string
+	Res        string
+	HttpOption bool
+	Path       string
+	HttpMethod string
+}
 
 func main() {
-	/* 这是我的第一个简单的程序 */
-	fmt.Println("Hello, World!")
+	data := `{
+		"Name":"datatest",
+		"RepoURL":"github.com/fish-repo",
+		"ID":27,
+		"MessageList":[
+			{
+				"Name":"fishmessageone",
+				"FieldList":[
+					{
+						"Name":"fishfieldone",
+						"Type":"string",
+						"ID":1,
+						"Repeated":false
+					},
+					{
+						"Name":"fishfieldtwo",
+						"Type":"int64",
+						"ID":2,
+						"Repeated":true
+					}
+				]
+			},
+			{
+				"Name":"fishmessagetwo",
+				"FieldList":[
+					{
+						"Name":"fishfieldthree",
+						"Type":"bool",
+						"ID":1,
+						"Repeated":true
+					},
+					{
+						"Name":"fishfieldfour",
+						"Type":"fishtype",
+						"ID":2,
+						"Repeated":false
+					}
+				]
+			}
+		],
+		"ServiceList":[
+			{
+				"Name":"Serviceone",
+				"Rpcs":[
+					{
+						"Name":"fisheat",
+						"Req":"fishmessageone",
+						"Res":"fishmessagetwo",
+						"HttpOption":true,
+						"Path":"/eat",
+						"HttpMethod":"post"
+					},
+					{
+						"Name":"fishsleep",
+						"Req":"fishmessagetwo",
+						"Res":"fishmessageone",
+						"HttpOption":false,
+						"Path":"/eat",
+						"HttpMethod":"post"
+					}
+				]
+			},
+			{
+				"Name":"egg",
+				"Rpcs":[
+					{
+						"Name":"eggeat",
+						"Req":"fishmessageone",
+						"Res":"fishmessageone",
+						"HttpOption":false,
+						"Path":"/eat",
+						"HttpMethod":"post"
+					},
+					{
+						"Name":"eggsleep",
+						"Req":"fishmessagetwo",
+						"Res":"fishmessagetwo",
+						"HttpOption":true,
+						"Path":"/sleep",
+						"HttpMethod":"get"
+					}
+				]
+			}
+		]
+	}`
+	createProtoRequest := &Proto{}
+	err := json.Unmarshal([]byte(data), createProtoRequest)
+	if err != nil {
+		log.Printf("[Create Proto]Request Json error: %#v", err)
+	}
+	fmt.Printf("%+v\n", createProtoRequest)
 }
